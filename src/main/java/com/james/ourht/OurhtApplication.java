986c757c1d058f57d729084b8d7124e7b79c6ff2
wnsgud0428 <wnsgud0428@naver.com>
2024-01-13 19:50:06 +0900

    setup

---
+++ src/main/java/com/james/ourht/OurhtApplication.java
@@ -0,0 +1,13 @@
+package com.james.ourht;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurhtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurhtApplication.class, args);
+	}
+
+}
