983331fdfdf1c26fa5ec8171c8436432d23c1547
wnsgud0428 <wnsgud0428@naver.com>
2024-01-16 22:47:58 +0900

    ExerciseRecord 테스트 만드는 중..

--- src/test/java/com/james/ourht/domain/exercise/ExerciseRecordServiceTest.java
+++ src/test/java/com/james/ourht/domain/exercise/ExerciseRecordServiceTest.java
@@ -1,4 +1,78 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.james.ourht.domain.exercise;
+
+import com.james.ourht.domain.member.Member;
+import com.james.ourht.domain.member.MemberRepository;
+import com.james.ourht.domain.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@SpringBootTest
+//@Transactional
 class ExerciseRecordServiceTest {
-  
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    MemberRepository memberRepository;
+    @Autowired
+    ExerciseRecordService exerciseRecordService;
+    @Autowired
+    ExerciseRecordRepository exerciseRecordRepository;
+
+
+    @Test
+    void 운동시작하고_멈추기() {
+        // given
+        Member member = createMember();
+
+        // when
+        Long recordId = exerciseRecordService.startExercise(member, "SQUAT");
+        try {
+            Thread.sleep(2500);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        exerciseRecordService.stopExercise(recordId);
+
+        // then
+        ExerciseRecord record = exerciseRecordRepository.findById(recordId).get();
+        Assertions.assertThat(record.getMember().getId()).isEqualTo(member.getId());
+        Assertions.assertThat(record.getEndedAt()).isNotNull();
+    }
+    
+    @Test
+    void 운동기록_전체조회() {
+        // given
+        Member member = createMember();
+
+        int exerciseCount = 10;
+        for (int i = 0; i < exerciseCount; i++) {
+            Long recordId = exerciseRecordService.startExercise(member, "SQUAT");
+            exerciseRecordService.stopExercise(recordId);
+        }
+
+        // when
+        List<ExerciseRecord> records = exerciseRecordService.findRecords();
+
+        // then
+        Assertions.assertThat(records.size()).isEqualTo(exerciseCount);
+
+    }
+
+    private Member createMember() {
+        Member member = new Member();
+        member.setName("김테스트");
+        member.setAge(25);
+        member.setEmail("test@gmail.com");
+        memberRepository.save(member);
+
+        return member;
+    }
+
 }
