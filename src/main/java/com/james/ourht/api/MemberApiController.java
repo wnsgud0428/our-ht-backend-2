f9ce25e58183045e55ac85e2b89226930ccd4cfd
wnsgud0428 <wnsgud0428@naver.com>
2024-01-20 21:30:39 +0900

    Meber 도메인 API 완성(회원 가입)

--- src/main/java/com/james/ourht/api/MemberApiController.java
+++ src/main/java/com/james/ourht/api/MemberApiController.java
@@ -1,9 +1,15 @@
 package com.james.ourht.api;
 
+import com.james.ourht.domain.member.Member;
 import com.james.ourht.domain.member.MemberService;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -11,14 +17,36 @@ public class MemberApiController {
     private final MemberService memberService;
 
     @PostMapping("/api/members")
-    public void saveMember() {
+    public CreateMemberResponse saveMember(@RequestBody CreateMemberRequest request) {
+        Member member = new Member();
+        member.setName(request.getName());
+        member.setEmail(request.getEmail());
+        member.setAge(request.getAge());
 
+        Long id = memberService.joinMember(member);
+        return new CreateMemberResponse(id);
     }
 
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1() {
+        return memberService.findMembers();
+    }
+
+
+    @Data
     static class CreateMemberRequest {
         private String name;
         private String email;
         private int age;
+    }
+
+    @Data
+    static class CreateMemberResponse {
+        private Long id;
+
+        public CreateMemberResponse(Long id) {
+            this.id = id;
+        }
     }
 
 
